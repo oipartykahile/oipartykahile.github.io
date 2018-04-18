d3f7d8c262de8fb14780c23d37582cb5c79af355
Subin <subeen.s2m@gmail.com>
2018-04-19 02:06:24 +0545

    changes in lower carousel

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,3 +7,17 @@ $(window).scroll(function(e) {
     
 });
 
+$('#party-palace-carousel').on('slide.bs.carousel', function(e) {
+    console.log(e.relatedTarget.id);
+    $('#carousel-text-1').css('display', 'none');
+    $('#carousel-text-2').css('display', 'none');
+    $('#carousel-text-3').css('display', 'none');
+    if(e.relatedTarget.id == 1) {
+        $('#carousel-text-1').css('display', 'block');
+    } else if (e.relatedTarget.id == 2) {
+        $('#carousel-text-2').css('display', 'block');
+    } else if (e.relatedTarget.id == 3) {
+        $('#carousel-text-3').css('display', 'block');
+    }
+});
+
