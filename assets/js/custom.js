b2d28b6685abc4e6af8891f368f4b662e2330662
Subin <subeen.s2m@gmail.com>
2018-04-19 02:49:28 +0545

    .

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,16 +8,16 @@ $(window).scroll(function(e) {
 });
 
 $('#party-palace-carousel').on('slide.bs.carousel', function(e) {
-    console.log(e.relatedTarget.id);
+    console.log(e);
     $('#carousel-text-1').css('display', 'none');
     $('#carousel-text-2').css('display', 'none');
     $('#carousel-text-3').css('display', 'none');
     if(e.relatedTarget.id == 1) {
-        $('#carousel-text-1').css('display', 'block');
+        $('#carousel-text-1').fadeIn('2000');
     } else if (e.relatedTarget.id == 2) {
-        $('#carousel-text-2').css('display', 'block');
+        $('#carousel-text-2').fadeIn('2000');
     } else if (e.relatedTarget.id == 3) {
-        $('#carousel-text-3').css('display', 'block');
+        $('#carousel-text-3').fadeIn('2000');
     }
 });
 
